f9ada0c36d5195b813a5f8c1a59520b4cbd28de5
sushmita98 <42720261+sushmita98@users.noreply.github.com>
2019-11-11 12:42:31 +0530

    Update doctor.js

--- models/doctor.js
+++ models/doctor.js
@@ -4,11 +4,15 @@ const doctorSchema = mongoose.Schema({
     fName: String,
     lName: String,
     image: String,
+    age: Number, 
+    sex: String, 
     specialization: String,
     qualification: String,
+    fees: Number, 
+    address: String, 
+    contact: Number, 
     in: String,
-    out: String,
-    fees: Number
+    out: String
 });
 
 const Doctor = mongoose.model('doctors', doctorSchema);
